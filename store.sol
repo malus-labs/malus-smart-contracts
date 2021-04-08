3456a9f532c43b7bd7c05f434fb5a6a0e96d335a
Warren Simpson <warren.simpson34@gmail.com>
2021-04-07 21:53:34 -0400

    Completed the mUSDC and store implementation.

--- store.sol
+++ store.sol
@@ -1,20 +1,23 @@
 pragma solidity ^0.8.0;
 //SPDX-License-Identifier: MIT
 
+
 abstract contract ERC20 {
     function balanceOf(address _owner) virtual public view returns (uint256 balance);
     function transferFrom(address _from, address _to, uint256 _amount) virtual public returns (bool success); 
 }
 
 
-interface StoreInterface {
-    function receiveCollateral(uint256 _amount, uint256 _rate, uint _option, bool _isTrade) external;
+abstract contract StoreHubInterface {
+    mapping(address => bool) public isValidStore;
+    mapping(address => uint256) public storeBalance;
+    function withdraw(uint256 _collateral) virtual external;
+    function callEvent(address _value1, uint256 _value2, uint256 _value3, bool _value4, uint _option) virtual external;
 }
 
 
-interface StoreHubInterface {
-    function withdraw(address _to) external;
-    function isStoreValid(address _store) external view returns (bool);
+interface StoreInterface {
+    function receiveCollateral(uint256 _amount, uint256 _rate, uint _option, bool _isTrade) external;
 }
 
 
@@ -64,6 +67,14 @@ contract Assets is Store {
         
         erc20Contract.transferFrom(address(this), _to, _amount);
     }
+    
+    function refreshStoreHubBalance(uint _option) public {
+        require(msg.sender == owner);
+        uint256 storeBalance = StoreHubInterface(storeHub[_option]).storeBalance(address(this)) - 1;
+        collateral[_option] += ((storeBalance * 700)/10000);
+        stake[_option] = 0;
+        StoreHubInterface(storeHub[_option]).withdraw(collateral[_option]);
+    }
 }
 
 
@@ -73,18 +84,15 @@ contract Stake is Assets {
         return (stake[_option], extension);
     }
     
-    function updateStake(uint256 _option, uint256 _amount, bool _addStake) external {
+    function updateStake(uint256 _amount, uint _option, bool _addStake) external {
         require(msg.sender == owner);
-        StoreHubInterface hub = StoreHubInterface(storeHub[_option]);
+
         if(_addStake == true) {
-            require(stake[_option] == 0);
-            stake[_option] = _amount;
-            
+            stake[_option] += _amount;
+            StoreHubInterface(storeHub[_option]).callEvent(address(0), stake[_option], collateral[_option], false, 1);
         }
         else {
-            stake[_option] = 0;
-            hub.withdraw(address(this));
-            
+            refreshStoreHubBalance(_option);
         }
     }
 }
@@ -104,40 +112,40 @@ contract Collateral is Stake {
             require(_getAvailableFunds(ERC20(address(aToken[_option])), _option) >= _amount);
             collateralRelief[_option][_rate] += _amount;
             totalRelief[_option] += _amount;
-            
+            StoreHubInterface(storeHub[_option]).callEvent(address(0), collateralRelief[_option][_rate], _rate, true, 0);
         }
         else {
             require(collateralRelief[_option][_rate] >= _amount);
             collateralRelief[_option][_rate] -= _amount;
             totalRelief[_option] -= _amount;
-            
+            StoreHubInterface(storeHub[_option]).callEvent(address(0), collateralRelief[_option][_rate], _rate, false, 0);
         }
     }
     
     function transferCollateral(StoreInterface _store, uint256 _amount, uint _option) external {
         require(msg.sender == owner);
-        require(StoreHubInterface(storeHub[0]).isStoreValid(address(_store)) == true);
+        require(StoreHubInterface(storeHub[0]).isValidStore(address(_store)) == true);
         require(collateral[_option] >= _amount);
         collateral[_option] -= _amount;
         _store.receiveCollateral(_amount, 0, _option, false);
         ERC20(aToken[_option]).transferFrom(address(this), address(_store), _amount);
-        
+        StoreHubInterface(storeHub[_option]).callEvent(address(0), stake[_option], collateral[_option], false, 1);
     }
     
     function sellCollateral(StoreInterface _store, uint256 _amount, uint256 _rate, uint _option) external {
         uint256 lost = (_amount * _rate) / 10000;
         require(msg.sender == owner);
-        require(StoreHubInterface(storeHub[0]).isStoreValid(address(_store)) == true);
+        require(StoreHubInterface(storeHub[0]).isValidStore(address(_store)) == true);
         require(lost >= 1);
         require(collateral[_option] >= _amount);
         collateral[_option] -= (_amount - lost);
         _store.receiveCollateral(_amount + lost, _rate, _option, true);
         ERC20(aToken[_option]).transferFrom(address(this), address(_store), lost);
-        
+        StoreHubInterface(storeHub[_option]).callEvent(address(0), stake[_option], collateral[_option], false, 1);
     }
     
     function receiveCollateral(uint256 _amount, uint256 _rate, uint _option, bool _isTrade) external {
-        require(StoreHubInterface(storeHub[0]).isStoreValid(address(msg.sender))  == true);
+        require(StoreHubInterface(storeHub[0]).isValidStore(address(msg.sender))  == true);
         
         if(_isTrade == true){
             require(collateralRelief[_option][_rate] == _amount);
@@ -145,11 +153,26 @@ contract Collateral is Stake {
             totalRelief[_option] -= _amount;
         }
         collateral[_option] += _amount;
-        
     }
     
-    function updateCollateral(uint256 _amount, uint256 _option) external {
+    function updateCollateral(uint256 _amount, uint _option) external {
         require(msg.sender == storeHub[_option]);
         collateral[_option] -= _amount;
+    }
+}
+
+
+contract General is Collateral {
+    
+    function updateExtension(address _newExtension, uint _option) external {
+        require(msg.sender == owner);
+        extension = _newExtension;
+        StoreHubInterface(storeHub[_option]).callEvent(extension, 0, 0, false, 2);
+    }
+    
+    function updateOwner(address _newOwner, uint _option) external {
+        require(msg.sender == owner);
+        owner = _newOwner;
+        StoreHubInterface(storeHub[_option]).callEvent(owner, 0, 0, false, 3);
     }
 }
