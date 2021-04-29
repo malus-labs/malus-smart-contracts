296567ba4c495605ac81ed7c6deeaeb6446def73
Warren Simpson <warren.simpson34@gmail.com>
2021-04-28 23:03:40 -0400

    The metadata contract was updated to ensure ensure only the owner of a store can set the metadata of that store.

--- metadata.sol
+++ metadata.sol
@@ -6,6 +6,9 @@ interface StoreHubInterface {
     function isStoreValid(address _store) external view returns (bool);
 }
 
+abstract contract StoreProxy {
+    address public owner;
+}
 
 contract Metadata {
     event MetaDataUpdated(address indexed store, string[7] metaData);
@@ -18,6 +21,7 @@ contract Metadata {
     
     function setMetaData(address _store, string[7] calldata _metaData) external {
         require(usdcHub.isStoreValid(_store) == true);
+        require(StoreProxy(_store).owner() == msg.sender);
         emit MetaDataUpdated(_store, _metaData);
     }
 }
